27bfb8bd22a6255b192abf6da60877d598b18436
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-24 09:52:33 +0530

    add main loop and handle user choice

--- main.c
+++ main.c
@@ -5,8 +5,30 @@ int main(){
   Customer customers;
   Booking Bookings;
 
+  // Main loop
   while (1){
-   
+    int choice;
+    main_menu();
+    printf("Please, enter your choice: ");
+    scanf("%d", &choice);
+
+    // Handle user choice
+    switch (choice){
+    case 1: car_manag_menu();
+      break;
+    case 2: customer_manag_menu();
+      break;
+    case 3: booking_manag_menu();
+      break;
+    case 4: printf("Exiting the program. Goodbye!\n");
+      return 0;
+
+    default:
+        if(choice < 1 || choice > 4){
+          printf("Invalid choice. Please try again.\n");
+        }
+      
+    }
   }
   
 
