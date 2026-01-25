8c766a947d460c737911ebe463e4228fedc30a5e
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-25 11:26:29 +0530

    car update menu define

---
+++ car_management/update_menu.c
@@ -0,0 +1,11 @@
+#include "../car_manag.h"
+
+
+void update_menu(){
+    printf("\n[1]. Car ID");
+    printf("\n[2]. Car Name");
+    printf("\n[3]. Car Price");
+    printf("\n[4]. Car Model");
+    printf("\n[5]. Car Availability");
+    printf("\n[0]. Back to menu");
+}
