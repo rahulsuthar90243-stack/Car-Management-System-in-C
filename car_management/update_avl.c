ae780a14d709e0a8bb84cbcde611f3c32b1ad72b
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-27 11:30:21 +0530

    update car model and file handling

---
+++ car_management/update_avl.c
@@ -0,0 +1,35 @@
+#include "../car_manag.h"
+
+void update_Availability(Car *car){
+    FILE *file = fopen("cars.dat", "r+b");
+    if(file == NULL){
+        printf("\nError opening file!\n");
+        return;
+    }
+
+    int id;
+    int found = 0;
+
+    printf("\nEnter Car Id: ");
+    scanf("%d", &id);
+
+    while (fread(car, sizeof(Car), 1, file)){
+        if(car->car_id == id){
+            printf("\nEnter Car Availability: ");
+            scanf("%d", &car->available);
+
+            fseek(file, -sizeof(Car), SEEK_CUR);
+            fwrite(car, sizeof(Car), 1, file);
+            fflush(file);
+            found = 1;
+            break;
+        }
+    }
+   if(found == 0 ){
+    printf("\nNo cars found!\n");
+   }else{
+    printf("\nCar ID update successfully.\n");
+    update_view(car);
+   }
+    fclose(file);
+}
