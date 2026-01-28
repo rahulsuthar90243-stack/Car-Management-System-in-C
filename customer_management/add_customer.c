dcc77e0ad4554db763b98b8cde36c87c68cc0837
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-28 11:22:09 +0530

    add_customer definition add use file handling store data customer in file

---
+++ customer_management/add_customer.c
@@ -0,0 +1,28 @@
+#include "../car_manag.h"
+
+void add_customer(Customer *customers){
+    FILE *file = fopen("customer.dat", "ab");
+    if(file == NULL){
+        printf("Error opening file!\n");
+        return;
+    }
+
+    printf("\nEnter Customer ID: ");
+    scanf("%d", &customers->customer_id);
+    getchar(); // to consume the newline character
+
+    printf("\nEnter Customer Name: ");
+    fgets(customers->customer_name, sizeof(customers->customer_name), stdin);
+    customers->customer_name[strcspn(customers->customer_name, "\n")] = 0; // Remove newline character
+
+    printf("\nEnter Phone Number: ");
+    fgets(customers->phone, sizeof(customers->phone), stdin);
+    customers->phone[strcspn(customers->phone, "\n")] = 0;
+    
+
+
+    fwrite(customers, sizeof(Customer), 1, file);
+    fclose(file);
+
+    printf("\nCustomer added successfully!\n");
+}
