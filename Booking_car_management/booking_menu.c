40e71d8f64e26b977f36311752acf24deb2b8743
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-29 12:33:34 +0530

    add booking menu function with switch case call another function

---
+++ Booking_car_management/booking_menu.c
@@ -0,0 +1,38 @@
+#include "../car_manag.h"
+
+Car car;
+Booking book;
+
+void booking_menu(){
+    int choice;
+    printf("\n===Booking Car Management==\n");
+    printf("[1].Book Car\n");
+    printf("[2].Return Car\n");
+    printf("[3].View Booking\n");
+    printf("[4].Back\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    // Here you can booking functionality based on the user's choice
+    switch(choice){
+        case 1:  book_car(&car, &book);
+        break;
+        case 2:  // Return_Car()
+        break;
+        case 3:   // View_Car()
+        break;
+        case 4:   //back
+        return;
+
+        default:
+            if(choice < 1 || choice > 4){
+                printf("Invalid choice . Please try again.\n");
+            }
+
+        }
+    }
+
+
+
+
