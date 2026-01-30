0b58187479924c33ad12f0c98342b5254e910e48
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-30 23:27:50 +0530

    add return car function and remove return car feature

---
+++ Booking_car_management/return_car.c
@@ -0,0 +1,44 @@
+#include "../car_manag.h"
+
+void return_car(){
+
+    Car cars;
+    Booking book;
+
+    FILE *file = fopen("booking.dat", "rb");
+    FILE *temp = fopen("temp.dat", "wb");
+
+    if(file == NULL || temp == NULL){
+     printf("\nError opening file!");
+    }
+
+    int book_id, found = 0;
+    int deleted = 0;
+    printf("\nPlease, enter booking Id: ");
+    scanf("%d", &book_id);
+
+    while (fread(&book, sizeof(Booking), 1, file)){
+        if(book.booking_id == book_id){
+         deleted = 1;
+        }else{
+            fwrite(&book, sizeof(Booking), 1, temp);
+            fflush(temp);
+        }
+    }
+
+    fclose(file);
+    fclose(temp);
+
+    if (!deleted){
+        remove("temp.dat");
+        printf("\nCar is now available for booking!\n");
+        return;
+    }
+
+    remove("booking.dat");
+    rename("temp.dat", "booking.dat");
+
+    printf("\nCar returned successfully.\n\n");
+
+    
+}
